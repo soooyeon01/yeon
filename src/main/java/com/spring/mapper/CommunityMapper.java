bcbfd1e876ce26f46e1e0e0aa5030535b9eb6d98
greenmurder <kki3103@gmail.com>
2023-07-10 17:24:17 +0900

    이것저것초기세팅

---
+++ src/main/java/com/spring/mapper/CommunityMapper.java
@@ -0,0 +1,12 @@
+package com.spring.mapper;
+
+import java.util.List;
+
+import com.spring.domain.CommunityDTO;
+
+public interface CommunityMapper {
+	CommunityDTO selectCommunity(int c_no);
+	int selectCountAllCommunity();
+	List<CommunityDTO> selectAllCommunityByPage(PageMaker pageMaker);
+
+}
---
+++ src/main/java/com/spring/util/Define.java
@@ -0,0 +1,5 @@
+package com.java.servlet.util;
+
+public class Define {
+	public static final int LOGIN_AUTH_SUCCESS = 1;
+}
