8bf5e72826a255cca7b8fc28137fd21d926522ba
kmj1000 <rlaalswnoo6@gmail.com>
2023-07-11 13:51:28 +0900

    ad

--- src/main/java/com/spring/controller/ShelterController.java
+++ src/main/java/com/spring/controller/ShelterController.java
@@ -6,28 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.service.S_Service;
+import com.spring.util.Criteria;
 import com.spring.util.PageMaker;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/shel/*")
+
+
 public class ShelterController {
 	private final S_Service service;
 	@GetMapping("/sheldetail")
-	public String getS(int shelter_no) {
-		service.getS(shelter_no);
+	public String getS(int shelter_no, Criteria cri, Model model) {
+		PageMaker pageMaker = new PageMaker(cri, cri.getAmount()); 
+		model.addAttribute("sheldetailList",service.getAllBoardByPage(pageMaker));
+		model.addAttribute("pageMaker",pageMaker);
 		return "/shel/sheldetail";
 	}
 	@GetMapping("/shelall")
-	public String getAllBoard() {
-		service.getAllBoard();
+	public String getAllBoard(Criteria cri, Model model) {
+		PageMaker pageMaker = new PageMaker(cri, cri.getAmount()); 
+		model.addAttribute("shelList",service.getAllBoardByPage(pageMaker));
+		model.addAttribute("pageMaker",pageMaker);
 		return "/shel/shelall";
 	}
-	@GetMapping("/shelpage")
-	public String getAllBoardByPage(PageMaker pageMaker, Model model) {
-		model.addAttribute("shelList",service.getAllBoardByPage(pageMaker));
-		return "/shel/shelpage";
-	}
+	
 }
