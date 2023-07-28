82afd29fbadfc6640c9d08dd78a30a7207c2e70d
2qiuo <2qiuo@naver.com>
2023-07-28 15:01:35 +0900

    d

--- src/main/java/com/spring/util/SendEmail.java
+++ src/main/java/com/spring/util/SendEmail.java
@@ -15,10 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class SendEmail {
 
-//	public static void main(String[] args) {
-//
-//	    naverMailSend(email, tempPwd);
-//	}
 	
 	public static void naverMailSend(String email, String tempPwd) {
         String host = "smtp.naver.com"; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
@@ -46,10 +42,11 @@ public class SendEmail {
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
 
             // 메일 제목
-            message.setSubject("임시 비밀번호 발급 안내");
+            message.setSubject("옥독캣 임시 비밀번호 발급 안내");
 
             // 메일 내용
-            message.setText("고객님의 임시비밀번호는 " + tempPwd + "입니다.");
+            message.setText("고객님의 임시비밀번호는 " + tempPwd + "입니다. \n"
+            		+ "로그인 하신 후 마이페이지>정보수정에서 반드시 비밀번호를 변경하여 주시기 바랍니다." );
 
             // send the message
             Transport.send(message);
