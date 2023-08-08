e65298508b71d5a5d0094d7cb0e6f05042462406
kmj1000 <rlaalswnoo6@gmail.com>
2023-08-08 20:04:14 +0900

    df

--- src/main/java/com/spring/controller/ApiPetDataController.java
+++ src/main/java/com/spring/controller/ApiPetDataController.java
@@ -62,17 +62,31 @@ public class ApiPetDataController {
 		try {
 			String url = "https://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?" + "pageNo=1&"
 					+ "numOfRows=1000&" + "serviceKey=" + serviceKey;
-
-			DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
+			
+			//xml 데이터를 파싱하기 위해 documentBuilder와 document 객체를 사용
+			//DocumentBuilderFactory는 xml 파서를 생성하기 위한 팩토리 클래스, DocumentBuilderFactory의 인스턴스를 생성해서 dbFactory에 저장
+			DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance(); 
+			
+			//DocumentBuilder는 xml문서를 파싱하는데 사용되는 클래스, DocumentBuilder의 인스턴스를 생성해서 dBuilder에 저장
 			DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
+			
+			//parse(url)메서드는 주어진 url에서 xml데이터를 가져와 파싱하여 doc에 저장
 			Document doc = dBuilder.parse(url);
+			
+			//xml 문서의 루트 요소를 얻고 정규화
 			doc.getDocumentElement().normalize();
-
+			
+			//doc.getElementsByTagName("totalCount") 을 호출하여 totalCount 태그를 가진 노드 목록을 가져와 totalNode에 저장.
 			NodeList totalNode = doc.getElementsByTagName("totalCount");
+			
+			// totalCount 태그의 이름을 가진 첫 번째 노드를 가져오는 과정을 설명
 			Element totalElement = (Element) totalNode.item(0);
+			// totalCount 태그의 값을 가져와 totalCount 변수에 저장합니다.
 			int totalCount = Integer.parseInt(totalElement.getTextContent());
 
 			int numOfRows = 1000;
+			
+			// 전체 페이지 개수(max)를 계산합니다.
 			max = (int) Math.ceil((double) totalCount / numOfRows);
 
 			// 바깥 for문을 추가하여 각 페이지의 데이터를 가져옵니다.
@@ -81,14 +95,20 @@ public class ApiPetDataController {
 						+ "&" + "numOfRows=1000&" + "serviceKey=" + serviceKey;
 				doc = dBuilder.parse(url);
 				doc.getDocumentElement().normalize();
-
+				
+				//doc.getElementsByTagName("item") 을 호출하여 item 태그를 가진 노드 목록을 가져와 nList에 저장한다.
 				NodeList nList = doc.getElementsByTagName("item");
-
+				
+				//temp 변수를 0부터 nList의 길이보다 작을 때까지 반복	
 				for (int temp = 0; temp < nList.getLength(); temp++) {
+					//nList에서 인덱스 temp에 해당하는 요소를 가져와서 nNode에 저장
 					Node nNode = nList.item(temp);
 					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-
+						
+						// 현재 nNode를 eElement로 캐스팅합니다.
 						Element eElement = (Element) nNode;
+						
+						// P_DTO 객체 pdto를 생성하고 각 태그의 값을 추출하여 설정합니다.
 						pdto.setHappenDt(getTagValue("happenDt", eElement));
 						pdto.setHappenPlace(getTagValue("happenPlace", eElement));
 						pdto.setKindCd(getTagValue("kindCd", eElement));
@@ -107,17 +127,22 @@ public class ApiPetDataController {
 						pdto.setCareAddr(getTagValue("careAddr", eElement));
 						pdto.setCareTel(getTagValue("careTel", eElement));
 						list.add(pdto);
+						
+						// 유기동물 공고 api 데이터 등록 메소드 실행
 						service.regitsterPetData(pdto);
 					}
 				}
 			}
+			// 유기동물 공고 api 중복 데이터 삭제 메소드 실행
 			service.removePetData(pdto);
+			// 유기동물 공고 api 종료된 데이터 삭제 메소드 실행
 			service.removePetEnd(pdto);
 			log.info("end");
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		// 실행 완료되면 api.jsp 페이지로 이동
 		return "/api/api";
 	}
 
@@ -129,13 +154,17 @@ public class ApiPetDataController {
 	 * @return 추출된 태그의 값 또는 찾지 못한 경우 null
 	 */
 	public static String getTagValue(String tag, Element eElement) {
+		// eElement에서 tag에 해당하는 NodeList의 첫 번째 요소에 접근합니다.
 		Node nlList = eElement.getElementsByTagName(tag).item(0);
+		
+		// 태그가 찾아졌는지 확인하고, 값이 있는 경우 그 값을 반환합니다.
 		if (nlList != null) {
 			Node nValue = nlList.getChildNodes().item(0);
 			if (nValue != null) {
 				return nValue.getNodeValue();
 			}
 		}
+		// 일치하는 태그 값이 없는 경우 null을 반환합니다.
 		return null;
 	}
 }
